fa0d14cc7db51e1fbb3657237943caec74fce48c
lgonzalezmislata <lgonzalez@fpmislata.com>
2021-01-05 13:25:46 +0100

    definitivo

--- src/web/index.php
+++ src/web/index.php
@@ -1,11 +1,7 @@
 <html>
 </body>
 <h1 style="color:green;background-color:blue">Servidor 1</h1>
-<p>Desplegar automaticamente</p>
-<p>otra prueba</p>
-<p>tercera prueba</p>
-<p>Cuarta prueba....</p>
-<p>Quinta prueba....</p>
+<p>Desplegar automaticamente con GitHub Actions</p>
 </body>
 </html>
 
