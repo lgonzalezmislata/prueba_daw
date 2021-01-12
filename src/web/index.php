70b1d87f315ae7543a194a544cf480143f05d087
lgonzalezmislata <lgonzalez@fpmislata.com>
2021-01-12 12:48:34 +0100

    comer

--- src/web/index.php
+++ src/web/index.php
@@ -1,7 +1,7 @@
 <html>
 </body>
 <h1 style="color:green;background-color:red">Servidor 1</h1>
-<p>Tengo dinero (bueno , no) y frio</p>
+<p>Tengo dinero (bueno , no) y frio y comer</p>
 </body>
 </html>
 
